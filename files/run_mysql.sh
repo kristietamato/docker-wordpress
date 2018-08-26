d3da31f786553b782af615ce9c49e2768c4d910c
Sam Gleske <sam.mxracer@gmail.com>
2018-08-25 23:42:19 -0700

    Improve security with randomly generated passwords

--- files/run_mysql.sh
+++ files/run_mysql.sh
@@ -8,15 +8,22 @@ if [ ! -d /var/lib/mysql/mysql ]; then
   INITIAL_ROOT_PASSWORD="${INITIAL_ROOT_PASSWORD:-$UUID_PASSWORD}"
   if [ "$INITIAL_ROOT_PASSWORD" = "$UUID_PASSWORD" ]; then
     # save the initial root password since it was generated
-    passfile=/mnt/mysql_pass
+    passfile=/mnt/mysql_root_pass
     touch "$passfile"
     chmod 600 "$passfile"
     echo "$INITIAL_ROOT_PASSWORD" > "$passfile"
     unset passfile
   fi
   WORDPRESS_USER="${WORDPRESS_USER:-wordpress}"
-  WORDPRESS_PASSWORD="${WORDPRESS_PASSWORD:-WordpressPassword}"
+  if [ "${WORDPRESS_PASSWORD:-default}" = default ]; then
+    WORDPRESS_PASSWORD="$(uuidgen)"
+  fi
   WORDPRESS_DATABASE="${WORDPRESS_DATABASE:-wordpress}"
+  touch /mnt/mysql_wordpress_user /mnt/mysql_wordpress_password /mnt/mysql_wordpress_database
+  chmod 600 /mnt/mysql_wordpress_user /mnt/mysql_wordpress_password /mnt/mysql_wordpress_database
+  echo "${WORDPRESS_DATABASE}" > /mnt/mysql_wordpress_database
+  echo "${WORDPRESS_USER}" > /mnt/mysql_wordpress_user
+  echo "${WORDPRESS_PASSWORD}" > /mnt/mysql_wordpress_password
   /usr/libexec/mariadb-prepare-db-dir
   /usr/bin/mysqld_safe --basedir=/usr &
   MYSQL_PID=$!
@@ -24,7 +31,6 @@ if [ ! -d /var/lib/mysql/mysql ]; then
   mysqladmin -u root password "$INITIAL_ROOT_PASSWORD"
   mysql -u root -p"$INITIAL_ROOT_PASSWORD" <<< ""
   mysql -u root -p"$INITIAL_ROOT_PASSWORD" <<EOF
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${INITIAL_ROOT_PASSWORD}' WITH GRANT OPTION;
 CREATE USER '${WORDPRESS_USER}'@'%' IDENTIFIED BY '${WORDPRESS_PASSWORD}';
 CREATE DATABASE ${WORDPRESS_DATABASE};
 GRANT ALL PRIVILEGES ON ${WORDPRESS_DATABASE}.* TO '${WORDPRESS_USER}'@'%' IDENTIFIED BY '${WORDPRESS_PASSWORD}';
